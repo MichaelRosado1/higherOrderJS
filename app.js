0ffb38f8e53e84f8f7557fc730318b18ecb83e18
MichaelRosado1 <mrosad4@illinois.edu>
2020-12-16 12:55:36 -0600

    working with closures and implemented a once function

--- app.js
+++ app.js
@@ -21,4 +21,36 @@ console.log(copyArrayAndDoSomething([1,2,3,4,5], addFiveToElement))
 //to increase legibility, the function itself can be a param
 
 console.log(copyArrayAndDoSomething([1,2,3,4,5], num => num + 100))
+//outputs: [101,102,103,104,105]
+
+
+//returns a function that will add param num to the first param x
+function addX(x) {
+	return function(num) {
+		return num + x
+	}
+}
+
+//function that allows inputted function to be ran only once
+function once(func) {
+	let count = 0;
+	let result = 0;
+	return function(num) {
+		if (count === 0) {
+			count++
+			result = func(num) 
+			return func(num)
+		}
+		return result
+	}
+}
+
+let addby4 = addX(4)
+
+let onceFunc = once(addby4)
+
+console.log(onceFunc(4))
+console.log(onceFunc(10))
+console.log(onceFunc(20))
+
 
